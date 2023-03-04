f0cb3c4c6c5d5167b1394bf390d4a965c51e7d67
이해민 <2haemin11@gmail.com>
2023-03-04 21:18:49 +0900

    App.js update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          travis success
+          travis success!!
         </a>
       </header>
     </div>
